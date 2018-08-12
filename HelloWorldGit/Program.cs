4798fced6386ea273f42e8accf8ef426ab75267c
Sergey SimpleCoder <simplecodechannel@gmail.com>
2018-08-12 16:18:18 +0300

    !!! added

--- HelloWorldGit/Program.cs
+++ HelloWorldGit/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!");
         }
     }
 }
